26bbb693b5df05ece155b3b453d4030cb356d141
MatiXxD <ilya_dinesenko@mail.ru>
2024-05-24 05:14:09 +0300

    Add tasks from level7 lection 7

---
+++ tasks/level7/lection7/Solution2.java
@@ -0,0 +1,8 @@
+package tasks.level7.lection7;
+
+public class Solution2 {
+    public static final String NAME = "Земля";
+    public static final double SQUARE = 510_100_000;
+    public static final long POPULATION = 7_594_000_000L;
+    public static final long EQUATOR_LENGTH = 40_075_696;
+}
